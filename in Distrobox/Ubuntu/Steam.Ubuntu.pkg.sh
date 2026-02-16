791381888638046db8326b006fc52aed48556cee
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2026-02-16 16:30:26 +0000

    Update Steam.Ubuntu.pkg.sh

--- in Distrobox/Ubuntu/Steam.Ubuntu.pkg.sh
+++ in Distrobox/Ubuntu/Steam.Ubuntu.pkg.sh
@@ -6,17 +6,4 @@ distrobox create --name $app --home ~/.uni/hyper/distrobox/HOME/$app --image $im
 
 distrobox enter $app
 
-installYay(){
-  echo https://github.com/Unicorn-OS/ArchLinux.Knowledge/blob/main/Package%20Manager/AUR/Yay/readme.md
-}
-
-echo """
-[multilib]
-Include = /etc/pacman.d/mirrorlist
-""" | sudo tee -a /etc/pacman.conf
-
-sudo pacman -Sy
-
-sudo pacman -S steam
-
 distrobox-export --app $bin
